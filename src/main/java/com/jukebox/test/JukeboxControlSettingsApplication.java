77d7864741f241c3cd1cc403b3ce21dc210c4c6b
Victor Perez <foxyuca@gmail.com>
2019-12-16 14:58:51 -0500

    Initial commit with project config files

---
+++ src/main/java/com/jukebox/test/JukeboxControlSettingsApplication.java
@@ -0,0 +1,13 @@
+package com.jukebox.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JukeboxControlSettingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JukeboxControlSettingsApplication.class, args);
+	}
+
+}
